5ee20d65534ea46ba566a81f9f0192486c27c918
samurai <panvovandrik@gmail.com>
2021-12-16 16:08:48 +0200

    add sorting

--- leaderboardFeature/src/main/java/com/panhuk/leaderboardfeature/ui/LeaderboardFragment.kt
+++ leaderboardFeature/src/main/java/com/panhuk/leaderboardfeature/ui/LeaderboardFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -69,49 +73,66 @@ class LeaderboardFragment : Fragment() {
   @Composable
   fun CreateLeaderboardFragment() {
     Column() {
-      CreateLeaderboardTitle()
-      CreateSort()
+      CreateLeaderboardTitleAndSort()
       CreateLazyColumn()
     }
   }
 
   @Composable
-  fun CreateLeaderboardTitle() {
-    Box(
-      contentAlignment = Alignment.TopCenter,
+  fun CreateLeaderboardTitleAndSort() {
+    Row(
       modifier = Modifier
         .fillMaxWidth()
-        .padding(top = 5.dp)
+        .padding(top = 5.dp),
+      horizontalArrangement = Arrangement.Center
     ) {
       Text(
         modifier = Modifier
           .border(BorderStroke(2.dp, Color.Blue))
-          .padding(8.dp),
+          .padding(start = 8.dp, end = 120.dp, bottom = 8.dp, top = 8.dp),
         text = stringResource(string.leaderboard),
         fontSize = 30.sp
       )
+      CreateSort()
     }
   }
 
   @Composable
   fun CreateSort() {
     val options = viewModel.sortTypes
-    var expanded by remember { mutableStateOf(false)}
-    var selectedOption by remember { mutableStateOf(options[0]) }
+    var expanded by remember { mutableStateOf(false) }
+    var selectedIndex by remember { mutableStateOf(0) }
 
-
-    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = !expanded }) {
-      Text(stringResource(selectedOption))
+    OutlinedButton(onClick = { expanded = true }) {
+      Text(
+        stringResource(options[selectedIndex]),
+        fontSize = 12.sp,
+        modifier = Modifier
+          .width(75.dp)
+          .align(Alignment.CenterVertically)
+      )
+      DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+      ) {
+        options.forEachIndexed { index, item ->
+          DropdownMenuItem(onClick = {
+            selectedIndex = index
+            expanded = false
+            viewModel.sort(item)
+          }) {
+            Text(stringResource(item))
+          }
+        }
+      }
     }
   }
 
   @Composable
   fun CreateLazyColumn() {
-    val leaderboards = remember { viewModel.leaderboard }
-
     LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
       items(
-        items = leaderboards,
+        items = viewModel.leaderboardSorted,
         itemContent = { LeaderboardListItem(leaderboard = it) }
       )
     }
--- leaderboardFeature/src/main/java/com/panhuk/leaderboardfeature/ui/LeaderboardViewModel.kt
+++ leaderboardFeature/src/main/java/com/panhuk/leaderboardfeature/ui/LeaderboardViewModel.kt
@@ -1,26 +1,36 @@
 package com.panhuk.leaderboardfeature.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.panhuk.leaderboardfeature.R
 import com.panhuk.leaderboardfeature.model.DataProvider
 import com.panhuk.leaderboardfeature.model.Leaderboard
 import javax.inject.Inject
 
 class LeaderboardViewModel @Inject constructor() {
-  val leaderboard: List<Leaderboard> = DataProvider.leaderboardList.sortedByDescending {
-    it.scoreLocalDate
-  }
+  private val leaderboard: List<Leaderboard> = DataProvider.leaderboardList
+  var leaderboardSorted by mutableStateOf(sortByTime())
 
   val sortTypes = listOf(R.string.sort_date, R.string.sort_score)
 
-  fun sortByScore(): List<Leaderboard> {
+  private fun sortByScore(): List<Leaderboard> {
     return leaderboard.sortedByDescending {
       it.score
     }
   }
 
-  fun sortByTime(): List<Leaderboard> {
+  private fun sortByTime(): List<Leaderboard> {
     return leaderboard.sortedByDescending {
       it.scoreLocalDate
+    }
+  }
+
+  fun sort(item: Int) {
+    leaderboardSorted = when (item) {
+      sortTypes[0] -> sortByTime()
+      sortTypes[1] -> sortByScore()
+      else -> sortByTime()
     }
   }
 }
