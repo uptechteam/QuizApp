8e04cb8573a977fcb90cee38fa921ae53320d24d
samurai <panvovandrik@gmail.com>
2021-12-21 15:29:14 +0200

    add firebase analytic

--- app/src/main/java/com/panhuk/quizapp/MainActivity.kt
+++ app/src/main/java/com/panhuk/quizapp/MainActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.panhuk.menufeature.MenuNavigator
 import com.panhuk.quizapp.databinding.ActivityMainBinding
+import com.panhuk.quizapp.di.MainActivityComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MenuNavigator {
   private lateinit var binding: ActivityMainBinding
   private lateinit var navigator: NavController
 
+  @Inject
+  protected lateinit var firebaseAnalytics: FirebaseAnalytics
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+    MainActivityComponent.create(applicationContext).inject(this)
     navigator = findNavController(R.id.nav_fragment)
   }
 
