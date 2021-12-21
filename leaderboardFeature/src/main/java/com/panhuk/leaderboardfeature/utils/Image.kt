a2ce9a7e6ee4b7ed41d21cc760cca4cf6fc4ae2c
samurai <panvovandrik@gmail.com>
2021-12-21 12:41:58 +0200

    fix PR

--- leaderboardFeature/src/main/java/com/panhuk/leaderboardfeature/utils/Image.kt
+++ leaderboardFeature/src/main/java/com/panhuk/leaderboardfeature/utils/Image.kt
@@ -3,18 +3,17 @@ package com.panhuk.leaderboardfeature.utils
 import com.panhuk.leaderboardfeature.R
 import kotlin.random.Random
 
-fun getDrawable(index: Int = Random.nextInt(11)): Int {
-  val drawables = listOf(
-    R.drawable.add1,
-    R.drawable.add2,
-    R.drawable.android1,
-    R.drawable.android2,
-    R.drawable.banana,
-    R.drawable.cash,
-    R.drawable.crown,
-    R.drawable.favorite,
-    R.drawable.goal,
-    R.drawable.handicrafts
-  )
-  return drawables[index]
-}
+private val drawables = listOf(
+  R.drawable.add1,
+  R.drawable.add2,
+  R.drawable.android1,
+  R.drawable.android2,
+  R.drawable.banana,
+  R.drawable.cash,
+  R.drawable.crown,
+  R.drawable.favorite,
+  R.drawable.goal,
+  R.drawable.handicrafts
+)
+
+fun getDrawableRes(): Int = drawables[Random.nextInt(drawables.size - 1)]
